a374f29d22ab7e8e293fbd721985a76d88c44d6b
paavo <shibaev.pp@phystech.edu>
2021-01-13 16:17:14 +0300

    add lambda config

--- db.go
+++ db.go
@@ -3,14 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 )
 
 func db() *mongo.Client {
-	godotenv.Load()
 	clientOptions := options.Client().ApplyURI(
 		"mongodb+srv://user:123@userscluster.whxir.mongodb.net/" +
 			"test?retryWrites=true&w=majority")
