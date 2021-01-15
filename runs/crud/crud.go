ef0d55590a0b7f55a125fded4347661049e26669
paavo <shibaev.pp@phystech.edu>
2021-01-15 14:06:18 +0300

    fix runners

--- runs/crud/crud.go
+++ runs/crud/crud.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/cheggaaa/pb/v3"
 	"log"
 	"math"
 	"net/http"
@@ -113,21 +114,51 @@ func sendDelete(postfix string, id int64) (d time.Duration) {
 	return
 }
 
+func meanVar(delta, squares, count int64) string {
+	avg_delta := float64(delta) / float64(count)
+	avg_square := float64(squares) / float64(count)
+	variance := avg_square - avg_delta*avg_delta
+	mean := avg_delta
+	return fmt.Sprintf("mean %f deviation %f", mean, math.Sqrt(variance))
+}
+
 func main() {
-	count := int64(12)
+	count := int64(200)
 	delta := int64(0)
 	square := int64(0)
+	bar := pb.StartNew(int(count))
+	var (
+		create_s, create_sq, read_s, read_sq, upd_s, upd_sq, del_s, del_sq int64
+	)
 	for i := int64(0); i < int64(count); i++ {
-		d1 := sendCreate("/create", i)
-		d2 := sendRead("/read", i)
-		d3 := sendUpd("/update", i)
-		d4 := sendDelete("/delete", i)
-		sq := d1.Microseconds() + d2.Microseconds() + d3.Microseconds() + d4.Microseconds()
+		d1 := sendCreate("/create", i).Microseconds()
+		d2 := sendRead("/read", i).Microseconds()
+		d3 := sendUpd("/update", i).Microseconds()
+		d4 := sendDelete("/delete", i).Microseconds()
+		sq := d1 + d2 + d3 + d4
 		delta += sq
 		square += sq * sq
+
+		create_s += d1
+		create_sq += d1 * d1
+		read_s += d2
+		read_sq += d2 * d2
+		upd_s += d3
+		upd_sq += d3 * d3
+		del_s += d4
+		del_sq += d4 * d4
+
+		bar.Increment()
 	}
-	avg_delta := float64(delta) / float64(count)
-	avg_square := float64(square) / float64(count)
-	fmt.Printf("Mean %v\n", avg_delta)
-	fmt.Printf("Var %v\n", math.Sqrt(avg_square-avg_delta*avg_delta))
+	bar.Finish()
+	fmt.Println("General CRUD")
+	fmt.Println(meanVar(delta, square, count))
+	fmt.Println("CREATE")
+	fmt.Println(meanVar(create_s, create_sq, count))
+	fmt.Println("READ")
+	fmt.Println(meanVar(read_s, read_sq, count))
+	fmt.Println("UPDATE")
+	fmt.Println(meanVar(upd_s, upd_sq, count))
+	fmt.Println("DELETE")
+	fmt.Println(meanVar(del_s, del_sq, count))
 }
